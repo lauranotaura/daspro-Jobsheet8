b74fe888d2c25b0ffd70f705930250a760cd71db
lauranotaura <nifa.aura11@smk.belajar.id>
2025-10-30 08:45:21 +0700

    tugas 2

--- NilaiKelompok21.java
+++ NilaiKelompok21.java
@@ -1,58 +1,71 @@
 import java.util.Scanner;
 
-public class NilaiKelompok21 {
+public class NilaiKelompok21{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i = 1;        
-        float nilai;      
-        float totalNilai;  
-        float rataNilai;   
 
-       
+        int i = 1;         
+        float nilai;        
+        float totalNilai;   
+        float rataNilai;    
+
+     
+        float maxRataRata = 0; 
+        int kelompokTerbaik = 0; 
+
+      
         System.out.println("==================================================");
         System.out.println(" PROGRAM PENILAIAN PROYEK KELOMPOK (6 Kelompok) ");
         System.out.println("==================================================");
         System.out.println("Harap masukkan 5 nilai (skala 1-100) untuk setiap kelompok.");
         System.out.println("--------------------------------------------------");
 
-       
         while (i <= 6) { 
-            
             System.out.println("\nKelompok " + i);
+            totalNilai = 0;
 
-            totalNilai = 0; 
-
-            
+         
             for (int j = 1; j <= 5; j++) {
                 System.out.print("Masukkan Nilai dari Kelompok Penilai " + j + ": ");
                 
-                
+         
                 if (sc.hasNextFloat()) {
                     nilai = sc.nextFloat();
                     if (nilai >= 1 && nilai <= 100) {
-                        totalNilai += nilai; 
+                        totalNilai += nilai;
                     } else {
                         System.out.println("⚠️ Nilai harus 1-100. Ulangi.");
-                        j--; 
+                        j--;
                     }
                 } else {
                     System.out.println("⚠️ Input tidak valid. Masukkan angka. Ulangi.");
-                    sc.next(); 
-                    j--; 
+                    sc.next();
+                    j--;
                 }
             }
             
-            
+          
             rataNilai = totalNilai / 5;
 
             System.out.println("Kelompok " + i + ": nilai rata-rata = " + String.format("%.1f", rataNilai));
+
+       
+            if (rataNilai > maxRataRata) {
+                maxRataRata = rataNilai;    
+                kelompokTerbaik = i;         
+            }
             
             i++; 
         }
 
-        System.out.println("\n==================================================");
-        System.out.println("Perhitungan selesai.");
         sc.close();
+        
+     
+        System.out.println("\n==================================================");
+        System.out.println("KELOMPOK DENGAN RATA-RATA NILAI TERTINGGI:");
+        System.out.println("🏆 Kelompok Terbaik adalah: Kelompok " + kelompokTerbaik);
+        System.out.println("   Dengan rata-rata nilai sebesar: " + String.format("%.1f", maxRataRata));
+        System.out.println("==================================================");
     }
 }
--- Tugas2.java
+++ Tugas2.java
@@ -2,44 +2,33 @@ import java.util.Scanner;
 public class Tugas2 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        System.out.print("Masukkan nilai N (minimal 3) = ");
+        int N = sc.nextInt();
 
-        String[] cabor = {"Badminton", "Tenis Meja", "Basket", "Bola Voli"};
-        final int JML_ATLET_PER_CABOR = 5;
-        final int JML_CABOR = cabor.length;
-
-        System.out.print("Masukkan jumlah Politeknik yang mendaftar: ");
-        int jmlPoliteknik = sc.nextInt();
-        sc.nextLine(); 
-
-        String[][][] dataPorseni = new String[jmlPoliteknik][JML_CABOR][JML_ATLET_PER_CABOR];
-
-        System.out.println("\n===== INPUT DATA ATLET PORSENI =====");
-
-        for (int p = 0; p < jmlPoliteknik; p++) {
-            System.out.println("\n--- Politeknik ke-" + (p + 1) + " ---");
-            for (int c = 0; c < JML_CABOR; c++) {
-                System.out.println("  Cabang: " + cabor[c]);
-                for (int a = 0; a < JML_ATLET_PER_CABOR; a++) {
-                    System.out.print("    Nama Atlet ke-" + (a + 1) + ": ");
-                    dataPorseni[p][c][a] = sc.nextLine();
-                }
-            }
+       
+        if (N < 3) {
+            System.out.println("Nilai N minimal harus 3.");
+            sc.close();
+            return;
         }
-        
-    
-        System.out.println("\n\n===== DAFTAR ATLET PORSENI 2024 =====");
 
-        for (int p = 0; p < jmlPoliteknik; p++) {
-            System.out.println("\n--- Data Politeknik ke-" + (p + 1) + " ---");
-            for (int c = 0; c < JML_CABOR; c++) {
-                System.out.println("  Cabang " + cabor[c] + ":");
-                for (int a = 0; a < JML_ATLET_PER_CABOR; a++) {
-                    System.out.println("    - Atlet ke-" + (a + 1) + ": " + dataPorseni[p][c][a]);
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                
+               
+                if (i == 1 || i == N || j == 1 || j == N) {
+                    System.out.print(N + " ");
+                } else {
+                    
+                    System.out.print("  "); 
                 }
             }
+            System.out.println(); 
         }
 
         sc.close();
     }
+
     
 }
